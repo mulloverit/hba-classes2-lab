c3a7316c68f153e316f8287cab325fd39d8702d2
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-17 16:37:32 -0800

    Melons.py Abstraction  Completed

---
+++ melons.py
@@ -0,0 +1,45 @@
+"""Classes for melon orders."""
+
+class AbstractMelonOrder():
+    """An abstract base calss tht other Melon Orders inherit from. """
+
+    def __init__(self, species, qty, order_type, tax):
+        self.species = species
+        self.qty = qty
+        self.shipped = False
+        self.order_type = order_type
+        self.tax = tax
+
+    def get_total(self):
+        """Calculate price, including tax."""
+
+        base_price = 5
+        total = (1 + self.tax) * self.qty * base_price
+
+        return total
+
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
+
+        self.shipped = True
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+
+    order_type = "domestic"
+    tax = 0.08
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+
+    order_type = "international"
+    tax = 0.17
+
+    def __init__(self, country_code):
+        self.country_code = country_code
+
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
